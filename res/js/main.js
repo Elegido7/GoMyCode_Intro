c7b1e184d085430220b20396a6f9a80b166e2ad9
Dennis <dennojunior@gmail.com>
2024-07-01 14:17:58 +0300

    linked CSS to html files and added rules for the HMTL data

---
+++ res/js/main.js
@@ -0,0 +1,19 @@
+const message = 'Hello world' 
+
+// Log to console
+console.log(message)
+
+console.log(7/5)
+console.log(7**2)
+console.log (6+20)
+console.log(258-48)
+console.log(21%3)
+console.log(10**309)
+var a=5
+var b=a
+var c=b
+console.log(c)
+var a=4
+var b=3
+a-=b
+console.log(a-=b)
